5fc157c619f95ded2230d1b002ec8366d107bcc6
rajvirkaur7 <rajvirkaur7250@gmail.com>
2025-03-19 17:21:36 +0530

    tt

---
+++ Basics.py
@@ -0,0 +1,117 @@
+# print("Hello Class")
+# print("How are you!", end=" ")
+# print("Hi")
+
+# #variables 
+# a=10
+# print(a)
+# # variable name always starts with alphabet
+# # vdfb
+
+# # print("hi")
+# b=23
+# c=33.5
+# d='Rajvir'
+# e=True  #or False
+
+# print(b)
+# print(c)
+# print(d)
+# print(e)
+
+# print("--------")
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+
+# # type casting or conversion
+# i=10
+
+# print(float(i))
+# j=9.9
+# print(int(j))
+# # s='dfjb'
+# # print(int(s))
+
+
+# user input -- input()  - by default input string 
+# var1= input("Enter var1 value ")
+# # var1= int(input("Enter var1 value "))
+# print(var1)
+
+
+# #indentation- extra space
+# print(type(var1))
+# print(var1+10)
+
+
+# Operators 
+
+# Arithmetic Operator
+# Relational or Comparison Operator
+# Logical Operator
+# Assignment Assignment 
+# MemberShip Operator 
+# Identity Operator 
+
+
+
+# Arithmetic Operator - +,-,*,/,**,%
+# a=int(input("Enter a "))
+# b=int(input("Enter b "))
+# print(a+b)
+# print(a*b)
+# print(a/b)
+# print(a-b)
+# print(a**b)
+# print(a%b)  # modulues -- remainder 
+
+# Assignment =,+=,-=,*=,/=
+a=10
+
+a+=20 #a=a+20
+
+a-=10 # a=a-10
+a*=5
+a/=50
+# print(a)
+
+# # Comparision operator ==,>,<,.=,<=,!= -- True or False
+# print(10>=20) 
+# print(10<=10) 
+# print(10==11) 
+# print(10>20)
+# print(34<40)
+
+# Logical Operator -- and or not
+
+# print(True or True)
+# print(True or False)
+# print(False or False)
+
+# print(True and True and True and False)
+
+# print(not True)
+
+# print(12>34 and 45<6 and 67>8)
+
+
+# Membership -- in , not in
+a="Gurminder"
+print("uri" in a)
+print("md" not in a)
+
+
+# Identity - is , is not 
+a=10
+b=20
+
+# n=int(input("type n "))
+# m=int(input("type m "))
+# print(n is m)
+
+
+# print(a is b)
+# print(a is not b)
